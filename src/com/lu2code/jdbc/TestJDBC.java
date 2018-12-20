0929b529250ebdeb09503d109f93e8fe7adb55b9
iMac <iJaya@iJay.local>
2018-12-20 09:27:27 -0500

    first commit

--- src/com/lu2code/jdbc/TestJDBC.java
+++ src/com/lu2code/jdbc/TestJDBC.java
@@ -9,7 +9,7 @@ public class TestJDBC {
 	public static void main(String[] args) {
 		
 		String jdbcUrl = 
-				"jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+				"jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
 		String user = "hbstudent";
 		String pass = "hbstudent";
 				
